dd7ebc10228e0d8ca8d0d35558a5b099f3b1bc63
RoccoMichel <122682577+RoccoMichel@users.noreply.github.com>
2025-12-16 14:47:52 +0100

    Gravity

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -9,9 +9,15 @@ public class PlayerMovement : MonoBehaviour
     private InputAction moveAction;
     private Vector2 moveValue;
     private Vector2 direction;
+    private Vector3 velocity;
+    private bool isGrounded;
     private float momentum;
+    private const float GRAVITY = -9.81f;
     public float accelerationSpeed = 4;
     public float decelerationSpeed = 2;
+    [SerializeField] private Transform groundCheck;
+    [SerializeField] private float groundDistance = 0.4f;
+    [SerializeField] private LayerMask groundMask;
 
     private void Start()
     {
@@ -29,9 +35,15 @@ public class PlayerMovement : MonoBehaviour
         moveValue = moveAction.ReadValue<Vector2>();
         momentum = Mathf.Clamp01(moveValue == Vector2.zero ? momentum - decelerationSpeed * Time.deltaTime : momentum + accelerationSpeed * Time.deltaTime);
         direction = Vector2.Lerp(direction, moveValue, 0.015f);
-
+                
         Vector3 move = momentum * playerStats.speed * (transform.right * direction.x + transform.forward * direction.y);
+        
+        isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);
+        if (isGrounded && velocity.y < 0) velocity.y = -2f;
+        velocity.y += GRAVITY * Time.deltaTime;
+
         controller.Move(Time.deltaTime * move);
+        controller.Move(Time.deltaTime * velocity);
     }
 
     public void SetSpeed(float f) => playerStats.speed = f;
